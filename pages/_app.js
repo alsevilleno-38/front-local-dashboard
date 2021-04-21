4fc01cc666f4ac050c7ac018087d17582d7c40a1
aliengamer38 <aliengamer38@gmail.com>
2021-04-21 12:07:25 +0800

    front

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import { Fragment } from 'react'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Fragment>
+			<Head>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<Component {...pageProps} />
+		</Fragment>
+	)
 }
 
 export default MyApp
